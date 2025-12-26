0dd7348d7e15753fa0fcc69f1115fa1a60c976e3
Leon Wohlgemuth <leonw@astecs.com>
2025-12-26 18:04:39 +0100

    Add database session management and image save endpoint to app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from services.image_service import ImageResource,ImageListResource
-from database.init import init_db
+from database.init import init_db, db_session
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,7 +10,14 @@ api = Api(app)
 api.add_resource(ImageResource, '/api/images/<int:image_id>')
 api.add_resource(ImageListResource, '/api/images')
 
+@app.route('/api/images/save', methods=['POST'])
+def save_image():
+    pass
+
 if __name__ == "__main__":
     init_db()
     app.run(debug=True)
-    
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
