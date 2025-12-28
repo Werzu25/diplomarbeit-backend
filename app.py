16427f68f249b23f86be04697a1bf87c35d182f6
Leon Wohlgemuth <leonw@astecs.com>
2025-12-28 13:03:58 +0100

    Add API resources for devices, predictions, and fill levels

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from services.image_service import ImageResource,ImageListResource
+from services.device_service import DeviceResource,DeviceListResource
+from services.prediction_service import PredictionResource,PredictionListResource
+from services.fill_level_service import FillLevelResource,FillLevelListResource
 from database.init import init_db, db_session
 
 from image_classification.modelTools import predict, train_model
@@ -11,6 +14,16 @@ api = Api(app)
 
 api.add_resource(ImageResource, '/api/images/<int:image_id>')
 api.add_resource(ImageListResource, '/api/images')
+
+api.add_resource(DeviceResource, '/api/devices/<int:device_id>')
+api.add_resource(DeviceListResource, '/api/devices')
+
+api.add_resource(PredictionResource, '/api/predictions/<int:prediction_id>')
+api.add_resource(PredictionListResource, '/api/predictions')
+
+api.add_resource(FillLevelResource, '/api/fill_levels/<int:fill_level_id>')
+api.add_resource(FillLevelListResource, '/api/fill_levels')
+
 
 @app.route('/api/images/predict', methods=['POST'])
 def predict_image():
