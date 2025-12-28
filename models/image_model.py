3d50db459852cba5114acab4933e466ecbe14a63
Leon Wohlgemuth <leonw@astecs.com>
2025-12-28 12:54:01 +0100

    Add relationship to PredictionModel in ImageModel for better data association

--- models/image_model.py
+++ models/image_model.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 from database.init import Base
+from models.prediction_model import PredicionModel
 
 class ImageModel(Base):
     __tablename__ = 'images'
@@ -9,6 +11,7 @@ class ImageModel(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     path: Mapped[str] = mapped_column(nullable=False)
     hash: Mapped[str] = mapped_column(nullable=False, unique=True)
+    prediction:Mapped["PredicionModel"] = relationship("PredicionModel", back_populates="image")
     width: Mapped[int] = mapped_column()
     height: Mapped[int] = mapped_column()
     creation_date: Mapped[str] = mapped_column()
