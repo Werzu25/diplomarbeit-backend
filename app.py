12c28f589abe2c66fe5d77822e1b4acac8e64b5d
Leon Wohlgemuth <leonw@astecs.com>
2025-12-28 15:53:00 +0100

    Implement image prediction functionality in predict_image endpoint

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+import io
+import base64
+
+from flask import Flask, request,jsonify,make_response
 from flask_restful import Resource, Api
+from PIL import Image
 
 from services.image_service import ImageResource,ImageListResource
 from services.device_service import DeviceResource,DeviceListResource
@@ -29,7 +33,23 @@ api.add_resource(FillLevelListResource, '/api/fill_levels')
 
 @app.route('/api/images/predict', methods=['POST'])
 def predict_image():
-    pass
+    content = request.get_json(silent=True)
+    predictions = []
+    if not content:
+        return make_response(jsonify({"message": "No input data provided"}), 400)
+    
+    if 'images' not in content:
+        return make_response(jsonify({"message": "No images provided"}), 400)
+    
+    for image in content['images']:
+        for cam, value in image.items():
+            img = Image.open(io.BytesIO(base64.decodebytes(bytes(value, "utf-8"))))
+            prediction = predict(img, "image_classification/model_weights.pth")
+            predictions.append({
+                "camera": cam,
+                "prediction": prediction
+            })
+    return make_response(jsonify(predictions), 200)
 
 @app.route('/api/images/save', methods=['POST'])
 def save_image():
