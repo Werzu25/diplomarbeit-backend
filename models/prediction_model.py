45e1a8c796eb11d3beccf09260738551da0149ed
Leon Wohlgemuth <leonw@astecs.com>
2025-12-28 12:54:56 +0100

    Add PredictionModel to represent prediction data with relationships to images and devices

---
+++ models/prediction_model.py
@@ -0,0 +1,33 @@
+import enum
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy import Enum
+
+from database.init import Base
+from models.image_model import ImageModel
+from models.device_model import DeviceModel
+
+class LabelTypes(enum.Enum):
+    GLASS = 1
+    PAPER = 2
+    PLASTIC = 3
+    METAL = 4
+
+class PredicionModel(Base):
+    __tablename__ = 'predictions'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    image_id: Mapped[int] = mapped_column(ForeignKey("images.id"))
+    image: Mapped["ImageModel"] = relationship("ImageModel", back_populates="prediction")
+    
+    device_id: Mapped[str] = mapped_column(ForeignKey("devices.id"))
+    device: Mapped["DeviceModel"] = relationship("DeviceModel", back_populates="predictions")
+
+    prediction_label: Mapped[LabelTypes] = mapped_column()
+    confidence: Mapped[float] = mapped_column()
+    real_label: Mapped[LabelTypes] = mapped_column(nullable=True)
+
+    def __repr__(self) -> str:
+        return f"Prediction(id={self.id}, image_id={self.image_id}, device_id='{self.device_id}', prediction_label={self.prediction_label}, confidence={self.confidence}, real_label={self.real_label})"
