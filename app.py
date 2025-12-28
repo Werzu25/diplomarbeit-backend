a5a58a40f2aa8f370b2e38e8419191bcb336729d
Leon Wohlgemuth <leonw@astecs.com>
2025-12-28 12:53:28 +0100

    Add predict_image route to handle image prediction requests

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from flask_restful import Resource, Api
 from services.image_service import ImageResource,ImageListResource
 from database.init import init_db, db_session
 
+from image_classification.modelTools import predict, train_model
+
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(ImageResource, '/api/images/<int:image_id>')
 api.add_resource(ImageListResource, '/api/images')
+
+@app.route('/api/images/predict', methods=['POST'])
+def predict_image():
+    pass
 
 @app.route('/api/images/save', methods=['POST'])
 def save_image():
