271a1559862f9c0ce98fc6ae85e69ce4d32f689a
Leon Wohlgemuth <leonw@astecs.com>
2025-12-29 17:41:22 +0100

    Refactor DeviceModel to use dataclass and update last_update field to be nullable

--- models/device_model.py
+++ models/device_model.py
@@ -1,24 +1,29 @@
+import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import DateTime
 from database.init import Base
+from dataclasses import dataclass
 
+@dataclass
 class DeviceModel(Base):
     __tablename__ = "devices"
+    
+    id:int
+    device_name:str
+    location:str
+    device_up:bool
+    last_update:datetime.datetime
 
     id: Mapped[int] = mapped_column(primary_key=True)
     device_name: Mapped[str] = mapped_column(nullable=False)
     location: Mapped[str] = mapped_column(nullable=True)
     device_up: Mapped[bool] = mapped_column(nullable=False, default=False)
-    last_update: Mapped[DateTime] = mapped_column(DateTime)
+    last_update: Mapped[DateTime] = mapped_column(DateTime,nullable=True)
 
     predictions: Mapped[list["PredictionModel"]] = relationship(
         "PredictionModel", back_populates="device"
     )
 
-    # siehe Punkt 2 (FillLevel) – das ist bei dir auch inkonsistent
     fill_level: Mapped["FillLevelModel"] = relationship(
         "FillLevelModel", back_populates="device", uselist=False
     )
-    
-    def __repr__(self) -> str:
-        return f"Device(id={self.id}, device_name='{self.device_name}', location='{self.location}', device_up={self.device_up}, last_update={self.last_update})"
