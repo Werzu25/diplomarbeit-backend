39b2df4e2dffe92e271f3082e55cee38ef826ca1
Leon Wohlgemuth <leonw@astecs.com>
2025-12-29 17:43:25 +0100

    Update ImageClassifierModel to use ResNet152 instead of ResNet50

--- image_classification/ImageClassifierModel.py
+++ image_classification/ImageClassifierModel.py
@@ -1,13 +1,13 @@
 from torch import nn
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet152, ResNet152_Weights
 
 
 class ImageClassifierModel(nn.Module):
     def __init__(self, num_classes=6):
         super(ImageClassifierModel, self).__init__()
         self.num_classes = num_classes
-        self.weights = ResNet50_Weights.DEFAULT
-        self.features = resnet50(weights=self.weights)
+        self.weights = ResNet152_Weights.DEFAULT
+        self.features = resnet152(weights=self.weights)
 
         in_features = self.features.fc.in_features
         self.features.fc = nn.Linear(in_features, num_classes)
