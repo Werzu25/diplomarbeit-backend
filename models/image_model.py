d8c208388a34627a84dc35148c6dbccac85dd2b4
Leon Wohlgemuth <leonw@astecs.com>
2026-01-04 11:48:04 +0100

    removed unused hash column and removed unnessecary __repr__ function

--- models/image_model.py
+++ models/image_model.py
@@ -10,18 +10,13 @@ class ImageModel(Base):
 
     id: int
     path: str
-    hash: str
     width: int
     height: int
     creation_date: datetime.datetime
 
     id: Mapped[int] = mapped_column(primary_key=True)
     path: Mapped[str] = mapped_column(nullable=False)
-    hash: Mapped[str] = mapped_column(nullable=False, unique=True)
     prediction:Mapped["PredictionModel"] = relationship("PredictionModel", back_populates="image")
     width: Mapped[int] = mapped_column()
     height: Mapped[int] = mapped_column()
     creation_date: Mapped[DateTime] = mapped_column(DateTime)
-
-    def __repr__(self) -> str:
-        return f"Image(id={self.id}, path='{self.path}', hash='{self.hash}', width={self.width}, height={self.height}, creation_date='{self.creation_date}')"
