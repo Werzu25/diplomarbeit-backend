fa36b5741235420106970b54c767bcabac31d010
Leon Wohlgemuth <leonw@astecs.com>
2026-01-04 11:47:17 +0100

    added uniqe device id colunm in device model

--- models/device_model.py
+++ models/device_model.py
@@ -1,20 +1,22 @@
 import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, String
 from database.init import Base
 from dataclasses import dataclass
 
 @dataclass
 class DeviceModel(Base):
     __tablename__ = "devices"
-    
-    id:int
-    device_name:str
-    location:str
-    device_up:bool
-    last_update:datetime.datetime
+
+    id: int
+    unique_device_id: str
+    device_name: str
+    location: str
+    device_up: bool
+    last_update: datetime.datetime
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    unique_device_id: Mapped[str] = mapped_column(String(128), nullable=False, unique=True, index=True)
     device_name: Mapped[str] = mapped_column(nullable=False)
     location: Mapped[str] = mapped_column(nullable=True)
     device_up: Mapped[bool] = mapped_column(nullable=False, default=False)
