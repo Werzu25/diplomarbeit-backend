b231b21c30627d33b8c5a83ce2d6053e63e77de1
Werzu25 <leonw@astecs.com>
2026-01-28 08:59:59 +0100

    Refactor prediction relationship in ImageModel to enhance clarity and maintainability

--- models/image_model.py
+++ models/image_model.py
@@ -8,7 +8,13 @@ class ImageModel(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     path: Mapped[str] = mapped_column(nullable=False)
-    prediction:Mapped["PredictionModel"] = relationship("PredictionModel", back_populates="image")
+    prediction: Mapped["PredictionModel"] = relationship(
+        "PredictionModel",
+        back_populates="image",
+        cascade="all, delete-orphan",
+        single_parent=True,
+        uselist=False,
+    )
     width: Mapped[int] = mapped_column()
     height: Mapped[int] = mapped_column()
     creation_date: Mapped[DateTime] = mapped_column(DateTime)
